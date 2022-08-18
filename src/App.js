f43e3dd30fdaa7f9875f211d20b530ae2a198377
Varun Kansal <varunkansal2111@gmail.com>
2022-08-18 22:53:58 +0530

    screens made

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import Row from "./Row";
-import requests from "./requests";
-import Banner from "./Banner"
-import Nav from "./Nav"
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+
 function App() {
+
+  const user=null;
   return (
     <>
-    <Nav/>
-    <Banner />
-    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
-    <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-    <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-    <Row title="Actions Movies" fetchUrl={requests.fetchActionMovies} isLargeRow={true} isSmallRow={true}/>
-    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow={true} isSmallRow={true}/>
-    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow={true} isSmallRow={true}/>
-    <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow={true} isSmallRow={true}/>
+      <BrowserRouter>
+        <Routes>
+          {
+            !user ?(
+              <Route exact path="/" element={<LoginScreen />} />
+
+            ) :(
+              <Route exact path="/" element={<HomeScreen />} />
+            )
+          }
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Row from "../Row";
+import requests from "../requests";
+import Banner from "../Banner"
+import Nav from "../Nav"
+
+function HomeScreen() {
+  return (
+    <>
+    <Nav/>
+    <Banner />
+    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+    <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+    <Row title="Actions Movies" fetchUrl={requests.fetchActionMovies} isLargeRow={true} isSmallRow={true}/>
+    <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow={true} isSmallRow={true}/>
+    <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow={true} isSmallRow={true}/>
+    <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow={true} isSmallRow={true}/>
+    </>
+  )
+}
+
+export default HomeScreen
