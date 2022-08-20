67badc9aef286e00fe131c635d779a498f4ec193
Varun Kansal <varunkansal2111@gmail.com>
2022-08-20 22:43:30 +0530

    doing

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -8,6 +8,26 @@ function SignupScreen() {
   };
   const signIn = (e) =>{
     e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({email: credentials.email, password: credentials.password})
+
+    });
+    console.log(credentials.email, credentials.password);
+    const json = await response.json();
+    if(json.success){
+      // Save the auth Token and redirect
+      localStorage.setItem('token', json.authToken);
+      navigate('/');
+    }
+    else{
+      alert('Please enter the valid credentials');
+    }
+    console.log(json);
+    
   }
   return (
     <div className='signupScreen'>
